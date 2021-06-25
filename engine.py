5bd9cc3e691e5302650349b97c01797e19f86533
stedeo <66328227+stedeo@users.noreply.github.com>
2021-06-25 16:11:39 -0600

    Part 3 Finished

--- engine.py
+++ engine.py
@@ -9,8 +9,14 @@ from render_functions import clear_all, render_all
 def main():
     screen_width = 80
     screen_height = 50
+
+    # size of map
     map_width = 80
     map_height = 45
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
@@ -21,17 +27,14 @@ def main():
     npc = Entity(int(screen_width /2), int(screen_height /2), '@', libtcod.yellow)
     entities = [npc, player]
 
-    libtcod.console_set_custom_font(
-        "arial10x10.png", libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD
-    )
+    libtcod.console_set_custom_font("arial10x10.png", libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    libtcod.console_init_root(
-        screen_width, screen_height, "libtcod tutorial revise", False
-    )
+    libtcod.console_init_root(screen_width, screen_height, "Roguelike Tutorial 2021", False)
 
     con = libtcod.console_new(screen_width, screen_height)
 
     game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
